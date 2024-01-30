d5a552deb561379116271de693ce68098a3c140d
Sistema2-14 <Sistema2-14@DESKTOP-9OOKO8G>
2024-01-30 16:18:03 -0600

    Agregar archivos de proyecto.

---
+++ DiaDelMes/Program.cs
@@ -0,0 +1,56 @@
+﻿
+{
+    Console.WriteLine("INTRODUZCA AQUI EL NUMERO DE DIA:");
+    int NDia = int.Parse(Console.ReadLine());
+    Console.WriteLine("INTRODUZCA AQUI EL NUMERO DE MES:");
+    int NMes = int.Parse(Console.ReadLine());
+
+    if (NDia>= 1 && NDia <= 31 && NMes >= 1 && NMes <= 12)
+    {
+        int DDA = 0;
+        switch (NMes)
+        {
+            case 1:
+                DDA = NDia;
+                break;
+            case 2:
+                DDA = NDia + 31;
+                break;
+            case 3:
+                DDA = NDia + 59;
+                break;
+            case 4:
+                DDA = NDia + 90;
+                break;
+            case 5:
+                DDA = NDia + 120;
+                break;
+            case 6:
+                DDA = NDia + 151;
+                break;
+            case 7:
+                DDA = NDia + 181;
+                break;
+            case 8:
+                DDA = NDia + 212;
+                break;
+            case 9:
+                DDA = NDia + 243;
+                break;
+            case 10:
+                DDA = NDia + 273;
+                break;
+            case 11:
+                DDA = NDia + 304;
+                break;
+            case 12:
+                DDA = NDia + 334;
+                break;
+        }
+        Console.WriteLine("El {0} del {1} es el dia {2} del año", NDia, NMes, DDA);
+    }
+    else
+    {
+        Console.WriteLine("Datos proporcionados no validos");
+    }
+}
---
+++ Tarea/Program.cs
@@ -0,0 +1,27 @@
+﻿{
+Console.Title = " El sexo de una persona";
+
+  Int32 edad;
+  String nombre, sexo;
+
+Console.WriteLine("Digitar tu nombre");
+  nombre = Console.ReadLine();
+
+Console.WriteLine("Digitar tu edad");
+  edad = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Digitar tu sexo ( Hombre=M y Mujer=F )");
+  sexo = Console.ReadLine();
+ 
+if (sexo == "M" || sexo == "m") // instruccion lógica OR
+{    
+  Console.WriteLine("\nEl Sr {0} ha sido registrado", nombre); // v
+}
+else
+{
+  Console.WriteLine("\nLa Sra {0} ha sido registrada", nombre); // f
+}
+  Console.WriteLine("\n\n");
+  Console.WriteLine("\n----->Fin del programa");
+  Console.ReadKey();
+}
